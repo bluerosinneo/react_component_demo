c6b1d86bdb64fb1d72786db4d34623702d316af6
Grimes <crameg@amazon.com>
2019-11-21 10:40:48 -0500

    finish rout and 404

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Welcome from './components/Welcome/Welcome';
 import Clock from './components/clock/Clock';
 import Contact from './components/contact/Contact';
 import Navigation from './components/navigation/Navigation';
+import FourOFour from './components/fourofour/FourOFour';
 
 // import the router component
 import { Route, Switch } from 'react-router-dom';
@@ -19,7 +20,7 @@ function App() {
           <Route path="/welcome/:name" component={Welcome} />
           <Route path="/clock" component={Clock} />
           <Route path="/contact" component={Contact} />
-          <Rout component={FourOFour} />
+          <Route component={FourOFour} />
         </Switch>
 
         {/* <Welcome name="Cramer" />
---
+++ src/components/fourofour/FourOFour.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './FourOFour.css';
+
+function FourOFour(){
+    return(
+        <div className="FourOFour">
+            <h1>Uh Oh!</h1>
+            <h1>404</h1>
+        </div>
+    )
+}
+
+export default FourOFour;
