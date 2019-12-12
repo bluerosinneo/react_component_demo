a25767e537a2255daa6dd088672ad25dd9eb0d9e
Grimes <crameg@amazon.com>
2019-12-12 16:56:42 -0500

    half normal

--- src/components/jeopardy/Jeopardy.js
+++ src/components/jeopardy/Jeopardy.js
@@ -6,6 +6,7 @@ class Jeopardy extends Component {
     constructor(props){
         super(props);
         this.client = new JeopardyService();
+        this.formData = {};
         this.state = {
             data: {},
             score: 0
@@ -29,13 +30,38 @@ class Jeopardy extends Component {
         this.getNewQuestion();
     }
 
+    submitAnswer = (event) => {
+        event.preventDefault();
+        console.log(this.formData.answer);
+        console.log(this.state.data.answer);
+        console.log(this.formData.answer === this.state.data.answer);
+        if(this.formData.answer === this.state.data.answer){
+            let tempScore = this.state.score;
+            tempScore = tempScore + this.state.data.value;
+            this.setState({
+                score: tempScore
+            })
+        }
+    }
+
+    handleChange = (event) => {
+        let formData = this.formData;
+        formData[event.target.name] = event.target.value;
+        this.formData = formData;
+    }
+
     render(){
+        console.log(this.state.data.answer);
+
+
         // conditional this.state.data does not work
         // since this.state.date exists at construction
         // so use this.state.data.id to see if actual data is there
         // is there somethign else we can use?
+        // console.log(this.state.data.category.title)
         if(this.state.data.id){
-            console.log(this.state.data.category.title) 
+            // console.log(this.state.data.category.title)
+            // console.log(this.state.data.happy + " &&&");
             return(
                 
                 <div>
@@ -43,10 +69,16 @@ class Jeopardy extends Component {
                     <div>-</div>
                     <div>Question</div>
                     <div>{this.state.data.question}</div>
-                    <div>Title</div>
+                    <div>Category</div>
                     <div>{this.state.data.category.title}</div>
                     <div>Value</div>
                     <div>{this.state.data.value}</div>
+                    <form onSubmit={this.submitAnswer}>
+                        <input onChange={this.handleChange} name="answer" />
+                        <button>Submit Answer</button>
+                    </form>
+                    <div>Score</div>
+                    <div>{this.state.score}</div>
                     
                     
                     {/* <span>{JSON.stringify(this.state.data[0])}</span> */}
